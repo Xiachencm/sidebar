08bcc59d45303355bd52f2f3147f0f031cb71a96
taMuazhale <yanbo0039@yeah.net>
2017-08-08 10:00:26 +0800

    Add files via upload

---
+++ js/service.js
@@ -0,0 +1,31 @@
+(function() {
+
+    // 返回顶部按钮自动隐藏
+    $(window).scroll(function(){
+        if ($(window).scrollTop()>200){
+            $('#goTop').fadeIn();
+        }else if($(window).scrollTop()<200){
+            $('#goTop').fadeOut();
+        }
+    });
+
+    $('#goTop').click(function() {
+        $("html, body").animate({scrollTop:0}, 200);
+    });
+
+
+
+    var music = document.getElementById("bgMusic");
+
+    $("#audioBtn").click(function(){
+
+        if(music.paused){
+            music.play();
+            $("#audioBtn").removeClass("pause").addClass("play");
+        }else{
+            music.pause();
+            $("#audioBtn").removeClass("play").addClass("pause");
+        }
+    });
+
+})();
